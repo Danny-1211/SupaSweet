4af8b9b22316dbd94f69e3cfb945ab4390d2ee1b
danny <danny21316@gmail.com>
2025-10-22 16:24:17 +0800

    products page init

---
+++ src/pages/Products.jsx
@@ -0,0 +1,14 @@
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+export function Products() {
+    return (
+        <>
+            <Header />
+            <main>
+
+            </main>
+            <Footer />
+        </>
+    )
+}
+export default Products;
